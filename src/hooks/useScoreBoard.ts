4c52090d597ca7f15032b3e092779f3df717aa4c
Andrei Biahun <andrei1biahun@gmail.com>
2025-09-09 18:56:41 +0200

    added ligic to score update

--- src/hooks/useScoreBoard.ts
+++ src/hooks/useScoreBoard.ts
@@ -1,23 +1,34 @@
-
-import { useState } from 'react';
-import type { Match } from '../types/Match';
+import { useState } from "react";
+import type { Match } from "../types/Match";
 
 export const useScoreboard = () => {
   const [matches, setMatches] = useState<Match[]>([]);
 
   const startNewMatch = (homeTeam: string, awayTeam: string) => {
     const newMatch: Match = {
-      id: Math.random().toString(36).substr(2, 9), 
+      id: Math.random().toString(36).substr(2, 9),
       homeTeam,
       awayTeam,
       homeScore: 0,
       awayScore: 0,
       startTime: Date.now(),
     };
-    setMatches(prevMatches => [...prevMatches, newMatch]);
+    setMatches((prevMatches) => [...prevMatches, newMatch]);
+  };
+
+  const updateScore = (
+    matchId: string,
+    homeScore: number,
+    awayScore: number
+  ) => {
+    setMatches((prevMatches) =>
+      prevMatches.map((match) =>
+        match.id === matchId ? { ...match, homeScore, awayScore } : match
+      )
+    );
   };
 
   const summary = matches;
 
-  return { summary, startNewMatch };
+  return { summary, startNewMatch, updateScore };
 };
