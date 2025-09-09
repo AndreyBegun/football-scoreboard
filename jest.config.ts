324c1d3bd3619226551cdd7bd9d0ac9c3d8bbcdf
Andrei Biahun <andrei1biahun@gmail.com>
2025-09-09 15:58:27 +0200

    initiated & configured react app with vite typescript jest react-testing-library eslint

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}','!src/vite-env.d.ts','!src/main.tsx'],
+};
+
+export default config;
