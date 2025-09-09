d629265a9f5b1bf67afa76f75d5cd11aea440399
Andrei Biahun <andrei1biahun@gmail.com>
2025-09-09 18:54:52 +0200

    added tests to score update

--- src/tests/scoreboard.test.tsx
+++ src/tests/scoreboard.test.tsx
@@ -1,22 +1,39 @@
+import { renderHook, act } from "@testing-library/react";
+import { useScoreboard } from "../hooks/useScoreBoard";
 
-import { renderHook, act } from '@testing-library/react';
-import { useScoreboard } from '../hooks/useScoreBoard';
-
-describe('useScoreboard', () => {
-  it('should start a new match with a score of 0-0', () => {
-    
+describe("useScoreboard", () => {
+  it("should start a new match with a score of 0-0", () => {
     const { result } = renderHook(() => useScoreboard());
-    
+
     act(() => {
-      result.current.startNewMatch('Mexico', 'Canada');
+      result.current.startNewMatch("Mexico", "Canada");
     });
 
     expect(result.current.summary.length).toBe(1);
     expect(result.current.summary[0]).toMatchObject({
-      homeTeam: 'Mexico',
-      awayTeam: 'Canada',
+      homeTeam: "Mexico",
+      awayTeam: "Canada",
       homeScore: 0,
       awayScore: 0,
     });
+  });
+
+  it("should update the score of an existing match", () => {
+	  const { result } = renderHook(() => useScoreboard());
+	  
+	  
+
+    act(() => {
+      result.current.startNewMatch("Spain", "Brazil");
+    });
+
+    const matchId = result.current.summary[0].id;
+
+    act(() => {
+      result.current.updateScore(matchId, 10, 2);
+    });
+
+    expect(result.current.summary[0].homeScore).toBe(10);
+    expect(result.current.summary[0].awayScore).toBe(2);
   });
 });
