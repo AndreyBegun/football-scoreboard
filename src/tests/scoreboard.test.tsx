996ed13df9de6cbb51debbf0a5b741d3c2c5adb8
Andrei Biahun <andrei1biahun@gmail.com>
2025-09-09 18:57:51 +0200

    added test to finish match

--- src/tests/scoreboard.test.tsx
+++ src/tests/scoreboard.test.tsx
@@ -19,9 +19,7 @@ describe("useScoreboard", () => {
   });
 
   it("should update the score of an existing match", () => {
-	  const { result } = renderHook(() => useScoreboard());
-	  
-	  
+    const { result } = renderHook(() => useScoreboard());
 
     act(() => {
       result.current.startNewMatch("Spain", "Brazil");
@@ -35,5 +33,22 @@ describe("useScoreboard", () => {
 
     expect(result.current.summary[0].homeScore).toBe(10);
     expect(result.current.summary[0].awayScore).toBe(2);
+  });
+
+  it("should finish a match and remove it from the scoreboard", () => {
+    const { result } = renderHook(() => useScoreboard());
+
+    act(() => {
+      result.current.startNewMatch("Germany", "France");
+    });
+
+    expect(result.current.summary.length).toBe(1);
+    const matchId = result.current.summary[0].id;
+
+    act(() => {
+      result.current.finishMatch(matchId);
+    });
+
+    expect(result.current.summary.length).toBe(0);
   });
 });
