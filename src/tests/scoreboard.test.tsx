a60d9a0da95dcd2407e3c5c52c8c3144a5661aab
Andrei Biahun <andrei1biahun@gmail.com>
2025-09-09 19:44:31 +0200

    added tests to sort the summary

--- src/tests/scoreboard.test.tsx
+++ src/tests/scoreboard.test.tsx
@@ -51,4 +51,47 @@ describe("useScoreboard", () => {
 
     expect(result.current.summary.length).toBe(0);
   });
+
+  it("should return summary ordered by total score and then by most recent start time", async () => {
+    const { result } = renderHook(() => useScoreboard());
+    const delay = () => new Promise((res) => setTimeout(res, 20));
+
+    await act(async () => {
+      result.current.startNewMatch("Mexico", "Canada");
+      await delay();
+      result.current.startNewMatch("Spain", "Brazil");
+      await delay();
+      result.current.startNewMatch("Germany", "France");
+      await delay();
+      result.current.startNewMatch("Uruguay", "Italy");
+      await delay();
+      result.current.startNewMatch("Argentina", "Australia");
+    });
+
+    act(() => {
+      const findId = (h: string, a: string) =>
+        result.current.summary.find(
+          (m) => m.homeTeam === h && m.awayTeam === a
+        )!.id;
+      result.current.updateScore(findId("Mexico", "Canada"), 0, 5);
+      result.current.updateScore(findId("Spain", "Brazil"), 10, 2);
+      result.current.updateScore(findId("Germany", "France"), 2, 2);
+      result.current.updateScore(findId("Uruguay", "Italy"), 6, 6);
+      result.current.updateScore(findId("Argentina", "Australia"), 3, 1);
+    });
+
+    const summaryTeams = result.current.summary.map(
+      (m) => `${m.homeTeam}-${m.awayTeam}`
+    );
+
+    const expectedOrder = [
+      "Uruguay-Italy",
+      "Spain-Brazil",
+      "Mexico-Canada",
+      "Argentina-Australia",
+      "Germany-France",
+    ];
+
+    expect(summaryTeams).toEqual(expectedOrder);
+  });
 });
